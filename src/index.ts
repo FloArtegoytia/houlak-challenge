5baea02162f65db0de879c96c624e86b1ed58169
FloArtegoytia <florencia@xperiencops.com>
2023-04-18 21:02:38 -0300

    Add post endpoint on backend and add cors policy

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { getAlbumsByArtist } from './spotifyAPI';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.post('/albums', async (req: Request, res: Response) => {
+  const artistName = req.body.name;
+
+  try {
+    const albums = await getAlbumsByArtist(artistName);
+
+    res.json(albums);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).send('Error al obtener los álbumes del artista');
+  }
 });
 
 app.listen(port, () => {
